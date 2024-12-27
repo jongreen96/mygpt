853ddf1de75292b9ba99006deb78545103809b07
Jon Green <jongreen1996@gmail.com>
2024-12-27 14:47:51 +0000

    Add placeholder hero header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { H1, H2, P } from '@/components/typography';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className='w-full h-dvh flex items-center justify-center'>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
-        My-GPT
-      </h1>
-    </div>
+    <header className='bg-primary-foreground text-center min-h-80 h-[30dvh] flex flex-col justify-center items-center border-dashed border-b'>
+      <H1>My-GPT</H1>
+      <H2>The AI Dashboard of Choice</H2>
+      <P className='!mt-0 text-muted-foreground max-w-prose'>
+        Used by #USERCOUNT# users, Generating #TOKENS# tokens and #IMAGECOUNT#
+        images.
+      </P>
+      <div className='mt-6 flex gap-2'>
+        <Button className='w-32'>Get Started</Button>
+        <Button className='w-32' variant='outline'>
+          Learn More
+        </Button>
+      </div>
+    </header>
   );
 }
