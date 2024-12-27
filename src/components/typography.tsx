1cdbb27376b632145fe7df8d6eb78422af42d2bc
Jon Green <jongreen1996@gmail.com>
2024-12-27 14:46:37 +0000

    Setup typography

---
+++ src/components/typography.tsx
@@ -0,0 +1,138 @@
+import { cn } from '@/lib/utils';
+
+export function H1({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1
+      className={cn(
+        'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function H2({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h2
+      className={cn(
+        'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+}
+
+export function H3({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h3
+      className={cn(
+        'scroll-m-20 text-2xl font-semibold tracking-tight',
+        className
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
+export function H4({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h4
+      className={cn(
+        'scroll-m-20 text-xl font-semibold tracking-tight',
+        className
+      )}
+    >
+      {children}
+    </h4>
+  );
+}
+
+export function P({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <p className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}>
+      {children}
+    </p>
+  );
+}
+
+export function Blockquote({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <blockquote className={cn('mt-6 border-l-2 pl-6 italic', className)}>
+      {children}
+    </blockquote>
+  );
+}
+
+export function Ul({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <ul className={cn('my-6 ml-6 list-disc [&>li]:mt-2', className)}>
+      {children}
+    </ul>
+  );
+}
+
+export function Code({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <code
+      className={cn(
+        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+        className
+      )}
+    >
+      {children}
+    </code>
+  );
+}
