58a413714cd1b4f8a80d55607c6e9e000e7308b1
Jon Green <jongreen1996@gmail.com>
2024-12-30 18:40:38 +0000

    Implement generic chat model using AI SDK

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,35 @@
-import { H1 } from '@/components/typography';
+'use client';
 
-export default async function ChatPage() {
+import { useChat } from 'ai/react';
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    maxSteps: 5,
+    onFinish: (message, { usage }) => {
+      console.log(usage);
+    },
+  });
   return (
-    <div className='w-full flex items-center justify-center h-dvh'>
-      <H1>Chat</H1>
+    <div className='flex flex-col w-full max-w-md py-24 mx-auto stretch border'>
+      {messages.map((m) => (
+        <div key={m.id} className='whitespace-pre-wrap'>
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.toolInvocations ? (
+            <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
+          ) : (
+            <p>{m.content}</p>
+          )}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className='fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl'
+          value={input}
+          placeholder='Say something...'
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 }
