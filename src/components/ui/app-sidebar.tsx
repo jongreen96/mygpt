a015112f93a9c085cb0fc6b7c96c734cf9dc7f59
Jon Green <jongreen1996@gmail.com>
2025-01-11 10:43:04 +0000

    Add ConversationInformation and ConversationDelete components; implement table for displaying conversation settings

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -11,8 +11,10 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import { ModelSettingsType } from '@/lib/ai-models';
 import { deleteConversation, getConversations } from '@/lib/db';
 import getSession from '@/lib/hooks/get-session';
+import { capitalize } from '@/lib/utils';
 import { MessageSquare, MoreHorizontalIcon, SquarePen } from 'lucide-react';
 import { revalidatePath } from 'next/cache';
 import Link from 'next/link';
@@ -44,6 +46,7 @@ import {
 } from './dropdown-menu';
 import { Separator } from './separator';
 import { CustomInternalTrigger } from './sidebar-trigger';
+import { Table, TableBody, TableCell, TableRow } from './table';
 import { ThemeToggle } from './theme-selector';
 import UserAvatar from './user-avatar';
 
@@ -120,58 +123,85 @@ function ConversationDropdownMenu({
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className='w-min'>
-        <DropdownMenuItem asChild>
-          <Dialog>
-            <DialogTrigger className='w-full rounded p-1 text-left text-sm'>
-              Information
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Conversation Information</DialogTitle>
-                <DialogDescription>
-                  Information about model settings as well as usage
-                </DialogDescription>
-              </DialogHeader>
+        <ConversationInformation settings={conversation.settings} />
 
-              {conversation.settings}
-            </DialogContent>
-          </Dialog>
-        </DropdownMenuItem>
-
-        <DropdownMenuItem asChild>
-          <AlertDialog>
-            <AlertDialogTrigger className='w-full rounded bg-destructive/50 p-1 text-left text-sm'>
-              Delete
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                <AlertDialogDescription>
-                  This action cannot be undone. This will permanently delete
-                  your conversation.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-
-                <form
-                  action={async () => {
-                    'use server';
-                    await deleteConversation(conversation.id);
-                    revalidatePath('/chat');
-                  }}
-                >
-                  <AlertDialogAction asChild>
-                    <button type='submit' className='w-full'>
-                      Delete
-                    </button>
-                  </AlertDialogAction>
-                </form>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </DropdownMenuItem>
+        <ConversationDelete id={conversation.id} />
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+function ConversationInformation({ settings }: { settings: string }) {
+  const conversationSettings = JSON.parse(settings) as ModelSettingsType;
+
+  return (
+    <DropdownMenuItem asChild>
+      <Dialog>
+        <DialogTrigger className='w-full rounded p-1 text-left text-sm'>
+          Information
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Conversation Information</DialogTitle>
+            <DialogDescription>
+              Information about model settings as well as usage
+            </DialogDescription>
+          </DialogHeader>
+
+          <Table>
+            <TableBody>
+              {Object.entries(conversationSettings)
+                .filter(([_, value]) => value !== null)
+                .map(([key, value]) => (
+                  <TableRow key={key}>
+                    <TableCell className='font-medium'>
+                      {capitalize(key)}
+                    </TableCell>
+                    <TableCell>{value}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </DialogContent>
+      </Dialog>
+    </DropdownMenuItem>
+  );
+}
+
+function ConversationDelete({ id }: { id: string }) {
+  return (
+    <DropdownMenuItem asChild>
+      <AlertDialog>
+        <AlertDialogTrigger className='w-full rounded bg-destructive/50 p-1 text-left text-sm'>
+          Delete
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              conversation.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+            <form
+              action={async () => {
+                'use server';
+                await deleteConversation(id);
+                revalidatePath('/chat');
+              }}
+            >
+              <AlertDialogAction asChild>
+                <button type='submit' className='w-full'>
+                  Delete
+                </button>
+              </AlertDialogAction>
+            </form>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </DropdownMenuItem>
   );
 }
