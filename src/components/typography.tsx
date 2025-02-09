7a2ad9dab03d894b8ea55319b8a3d334ba26cf97
Jon Green <jongreen1996@gmail.com>
2025-02-09 12:53:22 +0000

    Remove urgent TODO comment for image response generation in typography component

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -198,7 +198,6 @@ export const markdownComponents: Components = {
             customStyle={{ borderRadius: 4, fontSize: 14 }}
             {...props}
           >
-            {/* TODO: URGENT! Fix error when generating image responses */}
             {String(children).replace(/\n$/, '')}
           </SyntaxHighlighter>
         </div>
