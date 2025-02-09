38faec3d6d1fadbe0c87c10469ba02b5ff8b5363
Jon Green <jongreen1996@gmail.com>
2025-02-09 19:05:05 +0000

    Enhance homepage layout with new sections, buttons, and images for improved user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { H1, H2, P } from '@/components/typography';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { auth } from '@/lib/auth';
+import { BrainIcon, HandCoinsIcon, ImageIcon } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import deviceImage from '../../public/device-showcase-light.png';
+import heroImageLight from '../../public/mygpt-hero-image-light.png';
+import heroImageDark from '../../public/mygpt-hero-image.png';
 
 export default async function Home() {
   const session = await auth();
   if (session?.user) redirect('/chat');
 
   return (
-    <>
+    <main className='space-y-12'>
       <header className='sticky top-0 z-20 bg-background p-2'>
         <div className='mx-auto flex max-w-7xl items-center justify-between'>
           <H2 className='pb-0'>My-GPT</H2>
@@ -19,35 +31,118 @@ export default async function Home() {
         </div>
       </header>
 
-      {/* TODO: Page */}
-      {/* Fully up for removing this whole hero and starting fresh with a tried and tested design */}
-      <section className='relative z-10 flex h-[30dvh] min-h-80 flex-col items-center justify-center overflow-hidden border-b bg-primary-foreground p-2 text-center'>
-        <H1>My-GPT</H1>
-        <H2>The AI Dashboard of Choice</H2>
-        <P className='!mt-0 max-w-prose text-muted-foreground'>
-          Used by #USERCOUNT# users, Generating #TOKENS# tokens and #IMAGECOUNT#
-          images.
-        </P>
-        <div className='mt-6 grid grid-cols-2 gap-2'>
-          <Link href='/login' className={buttonVariants()}>
-            Get Started
-          </Link>
+      <section className='mx-auto grid min-h-[60dvh] max-w-7xl gap-10 px-2 sm:grid-cols-2 sm:gap-2'>
+        <div className='flex items-center justify-center'>
+          <div className='text-center sm:text-left'>
+            <H1 className='font-bold'>
+              Top AI Models, <br />{' '}
+              <span className='dark:bg-gradient-to-t dark:from-background dark:via-primary/75 dark:to-primary dark:bg-clip-text dark:text-transparent'>
+                No Monthly Subscription
+              </span>
+            </H1>
+            <P className='mx-auto max-w-[75%] text-muted-foreground sm:mx-0 sm:max-w-md'>
+              Access the top AI models without having to pay a monthly fee,
+              simply pay for what you use.
+            </P>
 
-          <Link
-            href='#pricing'
-            className={buttonVariants({ variant: 'outline' })}
-          >
-            Pricing
-          </Link>
+            <div className='mt-10 space-x-2'>
+              <Button asChild>
+                <Link href='/login'>Get Started</Link>
+              </Button>
+              <Button variant='outline'>Learn More</Button>
+            </div>
+          </div>
         </div>
 
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={'/logo-light.svg'}
-          alt='logo'
-          className='absolute -z-10 min-w-[800px] animate-spin-slow opacity-20'
+        <div className='hidden items-center sm:flex'>
+          <Image
+            src={heroImageLight}
+            alt='Hero Image'
+            className='hidden rounded dark:block'
+            priority
+          />
+          <Image
+            src={heroImageDark}
+            alt='Hero Image'
+            className='rounded dark:hidden'
+            priority
+          />
+        </div>
+      </section>
+
+      <section className='mx-auto grid max-w-5xl gap-6 px-2 text-center sm:grid-cols-3 sm:text-left'>
+        <Card>
+          <CardHeader>
+            <CardTitle className='flex items-center justify-between text-lg'>
+              Cutting Edge AI Models
+              <BrainIcon />
+            </CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+
+          <CardContent className='text-muted-foreground'>
+            Access the most advanced AI models without restrictions. Whether you
+            need powerful text generation, coding assistance, or deep AI
+            analysis, you&apos;ll always have the latest models at your
+            fingertips.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className='flex items-center justify-between text-lg'>
+              Generate AI Images
+              <ImageIcon />
+            </CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+
+          <CardContent className='text-muted-foreground'>
+            Create stunning AI-generated images in seconds. From concept art to
+            photorealistic visuals, our AI models bring your ideas to life with
+            precision and creativity—no design skills required.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className='flex items-center justify-between text-lg'>
+              Pay-As-You-Go
+              <HandCoinsIcon />
+            </CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+
+          <CardContent className='text-muted-foreground'>
+            No subscriptions. No hidden fees. Only pay for what you use. Get
+            full access to premium AI tools without committing to a costly
+            monthly plan—perfect for both casual users and power users.
+          </CardContent>
+        </Card>
+      </section>
+
+      <section className='mx-auto flex max-w-7xl flex-col items-center px-2 pt-10 text-center'>
+        <H2 className='pb-0'>Your AI, Anywhere You Need It</H2>
+        <P className='max-w-5xl'>
+          Stay connected across all your devices—desktop, tablet, or phone. Your
+          conversations sync seamlessly, so you can start a chat on your laptop
+          and continue right where you left off on your phone. Whether
+          you&apos;re at home, at work, or on the go, your AI is always with
+          you.
+        </P>
+
+        <Image
+          src={deviceImage}
+          alt='Device Image'
+          className='w-full max-w-5xl pt-2'
         />
       </section>
-    </>
+
+      <section className='mx-auto flex max-w-7xl flex-col items-center gap-6 px-2 pb-64 pt-10 text-center'>
+        <h2 className='flex text-center text-2xl font-bold tracking-tighter sm:text-6xl'>
+          So, the question is... <br /> What are you waiting for?
+        </h2>
+
+        <Button className='w-full max-w-prose'>Get Started</Button>
+      </section>
+    </main>
   );
 }
